5ae162add68843b7ec1fec407772b90decb7355d
thembani <thembani@live.co.uk>
2019-01-25 16:08:20 +0200

    gridsearch arguments

--- scripts/train_gridsearch_model.py
+++ scripts/train_gridsearch_model.py
@@ -33,14 +33,40 @@ from models import parameter_ffn_seq
 # ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Run dpr for streetbees datasets.')
+    parser.add_argument('-n','--neurons', required=True,
+                        type=int,
+                        metavar="64",
+                        help='Full path of the file the dqr will be run on.')
+    parser.add_argument('-t','--test', required=False,
+                        default=0.75,
+                        metavar="0.25",
+                        help='Test split for running grid search')
+    parser.add_argument('-c','--cpu', required=True,
+                        type=int,
+                        metavar="3",
+                        default=3,
+                        help='Cpu cores for parallelism.')
+
+    args = parser.parse_args()
+
+    print('Gridearch of {} neurons with {} test size'.format(args.neurons, args.test))
+
     print('Read data \n')
+    with open(os.path.join(DATA_PATH,'sentences.json'),'r') as datafile:
+        sentences = json.load(datafile)
+
     sentences_flat = raise_one_level(sentences)
     sentences_df = pd.DataFrame(sentences_flat)
 
     corpora_train, corpora_test, labels_train, labels_test = train_test_split(
                                                                         sentences_df['body'],
                                                                         sentences_df['class'],
-                                                                        test_size=0.25,
+                                                                        test_size=float(args.test),
                                                                         random_state=123)
     print("Training data \n")
     training_data = []
@@ -62,18 +88,18 @@ if __name__ == '__main__':
                    'attention': [128],
                   'input_shape': [document_matrix.iloc[0:5].shape[1]],
                   'nb_classes': [labels.iloc[0:5].shape[1]]}
-                  
-    for j in [64, 128, 256, 512, 1024, 2048]:
+
+    for j in [args.neurons]:
         for i in range(3):
             parameters['layers'].append([j]*(i+1))
 
-    ffn_grid = GridSearchCV(estimator=ffn, param_grid=parameters, n_jobs=-1, verbose=1)
+    ffn_grid = GridSearchCV(estimator=ffn, param_grid=parameters, n_jobs=args.cpu, verbose=1)
 
     grid_result = ffn_grid.fit(document_matrix, labels, epochs=100)
 
-    pd.DataFrame(grid_result.cv_results_).sort_values('mean_test_score', ascending=False).to_csv(os.path.join(MODELS_PATH, 'grid_result.pickle'))
+    pd.DataFrame(grid_result.cv_results_).sort_values('mean_test_score', ascending=False).to_csv(os.path.join(MODELS_PATH, 'grid_result_{}.csv'.format(args.neurons)))
 
-    with open(os.path.join(MODELS_PATH, 'grid_result.pickle'),'wb') as datafile:
+    with open(os.path.join(MODELS_PATH, 'grid_result_{}.pickle'.format(args.neurons)),'wb') as datafile:
         pickle.dump(grid_result, datafile)
 
     print('All done')
